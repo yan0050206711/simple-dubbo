d58b7a55fb3c5eeb2fb174037ab2b2d908ec048c
yancey <lysanguotstd@163.com>
2018-12-10 10:41:11 +0800

    客户端超时处理

--- src/test/java/com/tstd2/soa/test/service/UserServiceImpl.java
+++ src/test/java/com/tstd2/soa/test/service/UserServiceImpl.java
@@ -3,6 +3,11 @@ package com.tstd2.soa.test.service;
 public class UserServiceImpl implements UserService {
     @Override
     public String sayHello(String name) {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return "hello " + name;
     }
 }
