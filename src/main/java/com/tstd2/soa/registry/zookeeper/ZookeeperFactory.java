a2376282fdc065d39181c4bb12695f408d6d57f2
yancey <yanhuajian@apusapps.com>
2019-01-30 10:33:07 +0800

    注册zk临时节点

--- src/main/java/com/tstd2/soa/registry/zookeeper/ZookeeperFactory.java
+++ src/main/java/com/tstd2/soa/registry/zookeeper/ZookeeperFactory.java
@@ -9,7 +9,7 @@ public class ZookeeperFactory {
     public static CuratorFramework create(String address) {
         CuratorFramework client = CuratorFrameworkFactory.builder()
                 .connectString(address)
-                .sessionTimeoutMs(10000).retryPolicy(new ExponentialBackoffRetry(1000, 5))
+                .sessionTimeoutMs(1000).retryPolicy(new ExponentialBackoffRetry(1000, 5))
                 .build();
 
         client.start();
--- src/main/java/com/tstd2/soa/registry/zookeeper/ZookeeperRegistry.java
+++ src/main/java/com/tstd2/soa/registry/zookeeper/ZookeeperRegistry.java
@@ -9,12 +9,11 @@ import com.tstd2.soa.registry.NotifyListener;
 import com.tstd2.soa.registry.RegistryNode;
 import com.tstd2.soa.registry.ServerNode;
 import org.apache.curator.framework.CuratorFramework;
-import org.apache.zookeeper.data.Stat;
+import org.apache.zookeeper.CreateMode;
 import org.springframework.context.ApplicationContext;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 /**
  * zk注册中心处理类
@@ -22,6 +21,8 @@ import java.util.Set;
 public class ZookeeperRegistry implements BaseRegistry {
 
     private CuratorFramework zkClient;
+
+    private static final String BASE_PATH = "/simple_dubbo";
 
     @Override
     public boolean registry(String interfaceName, ApplicationContext application) {
@@ -67,9 +68,9 @@ public class ZookeeperRegistry implements BaseRegistry {
             Registry registry = application.getBean(Registry.class);
             this.createZk(registry.getAddress());
 
-            String path = "/" + interfaceName;
-            if (this.zkClient.checkExists().forPath(path) != null) {
-                List<String> nodes = this.zkClient.getChildren().forPath(path);
+            String interfacePath = BASE_PATH + "/" + interfaceName + "/providers";
+            if (this.zkClient.checkExists().forPath(interfacePath) != null) {
+                List<String> nodes = this.zkClient.getChildren().forPath(interfacePath);
                 List<RegistryNode> nodeList = new ArrayList<>();
                 for (String str : nodes) {
                     nodeList.add(JsonUtils.fromJson(str, RegistryNode.class));
@@ -98,14 +99,14 @@ public class ZookeeperRegistry implements BaseRegistry {
 
     private void sadd(String interfaceName, RegistryNode registryNode) throws Exception {
 
-        String path = "/" + interfaceName;
+        String interfacePath = BASE_PATH + "/" + interfaceName + "/providers";
 
-        if (this.zkClient.checkExists().forPath(path) != null) {
+        if (this.zkClient.checkExists().forPath(interfacePath) != null) {
 
             String host = registryNode.getProtocol().getHost();
             String port = registryNode.getProtocol().getPort();
 
-            List<String> nodeList = this.zkClient.getChildren().forPath(path);
+            List<String> nodeList = this.zkClient.getChildren().forPath(interfacePath);
 
             for (String nodeJson : nodeList) {
                 RegistryNode node = JsonUtils.fromJson(nodeJson, RegistryNode.class);
@@ -113,13 +114,16 @@ public class ZookeeperRegistry implements BaseRegistry {
                 // 是有有相同的机器，则删除后更新
                 if (host.equals(node.getProtocol().getHost()) && port.equals(node.getProtocol().getPort())) {
                     // 存在则更新
-                    this.zkClient.delete().forPath(path + "/" + nodeJson);
+                    this.zkClient.delete().forPath(interfacePath + "/" + nodeJson);
                     break;
                 }
             }
+        } else {
+            this.zkClient.create().creatingParentsIfNeeded().forPath(interfacePath);
         }
 
-        this.zkClient.create().creatingParentsIfNeeded().forPath(path + "/" + JsonUtils.toJson(registryNode));
+        // 创建临时节点，连接断开节点自动删除
+        this.zkClient.create().withMode(CreateMode.EPHEMERAL).forPath(interfacePath + "/" + JsonUtils.toJson(registryNode));
     }
 
 
