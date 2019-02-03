889ef9826108a82616627c0e8c694448bf0ef705
yancey <yanhuajian@apusapps.com>
2019-02-03 14:59:53 +0800

    hessian

--- src/main/java/com/tstd2/soa/remoting/http/server/DispatcherServlet.java
+++ src/main/java/com/tstd2/soa/remoting/http/server/DispatcherServlet.java
@@ -1,9 +1,10 @@
 package com.tstd2.soa.remoting.http.server;
 
-import com.tstd2.soa.common.JsonUtils;
+import com.tstd2.soa.common.serialize.hessian.HessianCodecUtil;
+import com.tstd2.soa.remoting.ResponseClientUtil;
 import com.tstd2.soa.remoting.exchange.model.Request;
 import com.tstd2.soa.remoting.exchange.model.Response;
-import com.tstd2.soa.remoting.ResponseClientUtil;
+import org.springframework.util.Base64Utils;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -16,14 +17,20 @@ public class DispatcherServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String requestJson = req.getParameter("q");
-        Request request = JsonUtils.fromJson(requestJson, Request.class);
+        // 解析请求参数
+        String q = req.getParameter("q");
+        byte[] reqBody = Base64Utils.decodeFromString(q);
+        Request request = (Request) HessianCodecUtil.decode(reqBody);
 
+        // 处理请求
         Response response = new Response();
         response = ResponseClientUtil.response(request, response);
 
+        // 响应结果
         PrintWriter writer = resp.getWriter();
-        writer.write(JsonUtils.toJson(response));
+        byte[] respBody = HessianCodecUtil.encode(response);
+        String result = Base64Utils.encodeToString(respBody);
+        writer.write(result);
         writer.flush();
         writer.close();
     }
