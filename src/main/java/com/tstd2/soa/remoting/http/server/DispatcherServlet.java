ce2808bbc225a2380dbec13fc6503809f5b83ac5
yancey <yanhuajian@apusapps.com>
2019-02-26 20:05:13 +0800

    servlet 3.1

--- src/main/java/com/tstd2/soa/remoting/http/server/DispatcherServlet.java
+++ src/main/java/com/tstd2/soa/remoting/http/server/DispatcherServlet.java
@@ -6,17 +6,62 @@ import com.tstd2.soa.remoting.exchange.model.Request;
 import com.tstd2.soa.remoting.exchange.model.Response;
 import org.springframework.util.Base64Utils;
 
+import javax.servlet.AsyncContext;
+import javax.servlet.ReadListener;
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class DispatcherServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        AsyncContext asyncContext = req.startAsync();
+
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) req.getServletContext().getAttribute("executor");
+
+        ServletInputStream inputStream = req.getInputStream();
+        inputStream.setReadListener(new ReadListener() {
+            @Override
+            public void onDataAvailable() throws IOException {
+
+            }
+
+            @Override
+            public void onAllDataRead() throws IOException {
+                executor.execute(() -> {
+                    try {
+                        String result = invoke(req);
+                        PrintWriter writer = asyncContext.getResponse().getWriter();
+                        writer.write(result);
+                        writer.flush();
+                        writer.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    asyncContext.complete();
+                });
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                asyncContext.complete();
+            }
+        });
+
+
+    }
+
+    private String invoke(HttpServletRequest req) throws IOException {
         // 解析请求参数
         String q = req.getParameter("q");
         byte[] reqBody = Base64Utils.decodeFromString(q);
@@ -27,12 +72,10 @@ public class DispatcherServlet extends HttpServlet {
         response = ResponseClientUtil.response(request, response);
 
         // 响应结果
-        PrintWriter writer = resp.getWriter();
         byte[] respBody = HessianCodecUtil.encode(response);
         String result = Base64Utils.encodeToString(respBody);
-        writer.write(result);
-        writer.flush();
-        writer.close();
+
+        return result;
     }
 
 }
